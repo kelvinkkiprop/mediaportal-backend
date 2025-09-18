68273174ad4b320262ae23c9b8d2c54c8b57b7e2
kelvinkkiprop <kelvinkiprop5@gmail.com>
2025-09-18 16:17:54 +0300

    DD

--- database/migrations/2025_09_06_171959_create_personal_access_tokens_table.php
+++ database/migrations/2025_09_06_171959_create_personal_access_tokens_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
+            // $table->morphs('tokenable');
             $table->uuid('tokenable_id');
             $table->text('name');
             $table->string('token', 64)->unique();
